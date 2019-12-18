287a82a889b04b4853e6e2823301a469489a9658
deyantiherliana <deyantiherliana03@gmail.com>
2019-12-18 19:09:03 +0700

    PERTEMUAN12-13

---
+++ 08-Variable/Main.Java/src/main/java/MainJava.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java;
+
+/**
+ *
+ * @author Deyanti Herliana
+ */
+public class MainJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // kita akan membuat variabel
+        // tipe data
+        int a = 10; // assignment
+        System.out.println("nilai a = " + a);
+
+        a = 20;
+        System.out.println("nilai a baru = " + a);
+
+        // kita akan membuat sebuah deklarasi
+        int b; // deklarasi
+
+        b = 7;
+
+        System.out.println("nilai b = " + b);
+    }
+    
+}
